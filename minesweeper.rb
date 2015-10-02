d0c31fb00cc7ef356276cec49504b675fff07f88
Jake Nelken <jnelks@gmail.com>
2015-10-02 13:52:35 -0400

    create board#render and fixed board#create_bomb

--- minesweeper.rb
+++ minesweeper.rb
@@ -3,12 +3,17 @@ require 'byebug'
 class Board
   attr_reader :grid
 
-  BOARD_SIZE = 9
-
-  def initialize(grid = Array.new(9) {Array.new(9) {TileNode.new} }, difficulty = :normal)
-    @grid = grid
-    @difficulty = difficulty
+  def initialize(difficulty = :normal)
+    @grid = create_grid(difficulty)
     create_bombs
+  end
+
+  def create_grid(difficulty)
+    case difficulty
+    when :easy; Array.new(6) {Array.new(6) {TileNode.new}}
+    when :normal; Array.new(9) {Array.new(9) {TileNode.new}}
+    when :hard; Array.new(12) {Array.new(12) {TileNode.new}}
+    end
   end
 
   def [](pos) #[1,0]
@@ -25,8 +30,8 @@ class Board
   def create_bombs
     bombs = num_of_bombs
 
-    0.upto(BOARD_SIZE - 1) do |x|
-      0.upto(BOARD_SIZE - 1) do |y|
+    @grid.each_index do |x|
+      @grid[x].each_index do |y|
         pos = [x,y]
 
         unless self[pos].value == :b
@@ -38,37 +43,73 @@ class Board
         end
       end
     end
-    
+
     nil
   end
 
   def create_bomb?
-    rand(0..1) == 1 ? true : false
+    rand(1..3) == 1 ? true : false
   end
 
 
   def num_of_bombs
-    case @difficulty
-    when :normal
-      num_of_bombs = (@grid.length*@grid.length) / 3 # 1/3 of board are bombs
-    end
-     num_of_bombs
+    num_of_bombs = (@grid.length*@grid.length) / 3 # 1/3 of board are bombs
   end
+
+  def render
+    @grid.each_index do |x|
+      @grid[x].each_index do |y|
+        pos = [x,y]
+        if y == @grid.length - 1
+          print "[#{self[pos].value}]\n"
+        else
+          print "[#{self[pos].value}]"
+        end
+      end
+    end
+    nil
+  end
+
 end
 
 class TileNode
   attr_accessor :value
 
-  def initialize(value = nil)
+  def initialize(value = " ")
     @value = value
   end
 
 end
 
 class Game
+
+  def initialize(board = Board.new)
+    @board = board
+  end
+
+
+  def reveal(pos)
+    @board[pos].value
+  end
+
+  def play
+    until @board.over?
+      render
+      play = gets.chomp
+      render
+      if play  == :b
+        return game_over
+      end
+    end
+  end
+
+  def game_over
+    puts "Game Over"
+  end
+
 end
 
 if __FILE__ == $PROGRAM_NAME
   a = Board.new
-  p a
+  a.render
 end
